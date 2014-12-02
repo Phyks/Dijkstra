4e9395ac5a483e83bf1095f036b169accbad6e94
cphyc <admin@cphyc.me>
2014-12-02 18:33:38 +0100

    added a subfunction to explore neighbours

--- dijkstra.c
+++ dijkstra.c
@@ -1,46 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "dijkstra.h"
 #include "queue.h"
 #include "states.h"
+#include "utils.h"
 #define UNDEF -1
+
+void exploreNeighbours(graph_t* G, priority_queue_t* P, int* d_s, int start) {
+  adjacency_list_node_t* ngbr;
+  int n;
+  priority_queue_element_t* tmp;
+
+  /* start -> ngbr */
+  ngbr = G->adjacency_list_array[start].head;
+  /* n neighbours */
+  n = G->adjacency_list_array[start].nb_members;
+  for (int i = 0; i < n; i++) {
+    /* allocate memory for tmp */
+    tmp = (priority_queue_element_t*)
+      safe_malloc(sizeof(priority_queue_element_t));
+    tmp->u    = start;
+    tmp->edge = ngbr;
+    tmp->length = d_s[start] + ngbr->weight;
+    priorityQueueInsert(P, tmp);
+    /* next neighbour */
+    ngbr     = ngbr->next;
+  }
+
+  free(ngbr);
+}
 
 int* dijkstra(graph_t* G, int s){
   priority_queue_t P;
   priority_queue_element_t tmp;
 
-  adjacency_list_node_t* vertex;
-  int u, v, w, n, length;
-  adjacency_list_node_t* e;
+  int u, v, w;
 
-  int* d_s = (int*) malloc(sizeof(int)*G->nb_vertices);
+  int* d_s = (int*) safe_malloc(sizeof(int)*G->nb_vertices);
   states_t *states = initStates(G);
 
   P.nb_members = 0;
-  P.elements = NULL;
+  P.elements   = NULL;
 
+  /* for all nodes of the graph s */
   for (int i = 0; i < G->nb_vertices; i++) {
-    if (i == s) {
+    if (i == s) 		/* distance from start point = 0 */
       d_s[i] = 0;
-    }
-    else {
+    else 
       d_s[i] = UNDEF;
-    }
   }
 
   markNode(states, s, VISITED);
 
   /* Iterate over all edges going out of s */
-  e = G->adjacency_list_array[s].head;
-  n = G->adjacency_list_array[s].nb_members;
-  for (int i = 0; i < n; i++) {
-    length = d_s[s] + e->weight;
-    tmp.u = s;
-    tmp.edge = e;
-    tmp.length = length;
-    priorityQueueInsert(&P, &tmp);
-    e = e->next;
-  }
+  exploreNeighbours(G, &P, d_s, s);
 
   while (!priorityQueueIsEmpty(&P)) {
     tmp = *priorityQueueExtractMin(&P);
@@ -52,19 +66,9 @@ int* dijkstra(graph_t* G, int s){
       d_s[v] = w;
       markNode(states, v, VISITED);
 
-      vertex = G->adjacency_list_array[v].head;
-      n = G->adjacency_list_array[v].nb_members;
-      for (int i = 0; i < n; i++) {
-        length = d_s[u] + vertex->weight;
-        tmp.u = u;
-        tmp.edge = vertex;
-        priorityQueueInsert(&P, &tmp);
-        vertex = vertex->next;
-      }
+      exploreNeighbours(G, &P, d_s, v);
     }
   }
-  free(vertex);
-  free(e);
   return d_s;
 }
 
