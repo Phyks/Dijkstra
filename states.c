9ac1cd922ec6247197a39ce049a3bcefc434dfe1
Phyks <webmaster@phyks.me>
2014-12-06 19:53:21 +0100

    Fix incorrect isState function in states

--- states.c
+++ states.c
@@ -36,7 +36,7 @@ states_t *initStates(graph_t const *graph) {
  * Free the states array.
  */
 void freeStates(states_t *states) {
-  free(states->states);  
+  free(states->states);
   free(states);
 }
 
@@ -50,9 +50,17 @@ void markNode(states_t *states, int v, state_t state) {
 
 
 /**
- * Check if node is already of given state.
+ * Check if the node `v` is already of given state.
  */
 int isState(states_t const *states, int v, state_t state) {
+  return (states->states[v] == state);
+}
+
+
+/**
+ * Check if *a* node is already of given state.
+ */
+int someIsState(states_t const *states, state_t state) {
   for (int i = 0; i < states->nb_members; i++) {
     if (states->states[i] == state) {
       return true;
--- states.h
+++ states.h
@@ -19,5 +19,6 @@ states_t *initStates(graph_t const *graph);
 void freeStates(states_t *states);
 void markNode(states_t *states, int v, state_t state);
 int isState(states_t const *states, int v, state_t state);
+int someIsState(states_t const *states, state_t state);
 
 #endif  // _STATES_H_
