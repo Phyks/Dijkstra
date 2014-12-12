80af39cffa719b303faad5e6e1d7b062bd4350ff
cphyc <admin@cphyc.me>
2014-12-12 12:12:46 +0100

    Using generic fonction to parse the osm file

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ def degToRad(deg):
     return deg/360*2*pi
 
 class osmMap:
-    nodes = {}
+    nodes = dict()
     roads = []
+    # stores the distances between two nodes u<v as two lists:
+    # distances[u] = ([v0, v1, …], [d0, d1, …])
+    distances = dict()         
 
     def __init__(self, osm_file):
         try:
@@ -36,14 +39,32 @@ class osmMap:
         # get all the roads (way with child that has the attribute k = hway)
         for way in root.findall('.//way/*[@k="highway"]/..'):
             self.roads.append(way)
-
-    def printGraph(self):
-        # create the graph in the format
-        self.graph = ""
-        edges = ""
-        n_nodes = len(self.nodes)
-        n_edges = 0
+            
+    def distance(self, u, v):
         R = 6371000
+        lat1 = degToRad(float(u.get("lat"))) / 2
+        lat2 = degToRad(float(v.get("lat"))) / 2
+        lon1 = degToRad(float(u.get("lon"))) / 2
+        lon2 = degToRad(float(v.get("lon"))) / 2
+
+        dlat = lat2-lat1
+        dlon = lon2-lon1
+        a = (pow(sin(dlat)/2, 2)
+             + cos(lat1)*cos(lat2)*pow(sin(dlon)/2, 2))
+        c = 2*atan2(sqrt(a), sqrt(1-a))
+        dist = R*c
+        return dist
+    
+    def parse(self, funNNode, funAdd):
+        """ This method runs through the data and applies funNNode to the number
+        of nodes and funAdd for each segment of each way. It doesn't return
+        anything.
+
+        funNNode: takes a number of node
+        funAdd: takes two nodes and a distance
+        """
+
+        funNNode(len(self.nodes))
 
         # iterate over all ways
         for road in self.roads:
@@ -52,35 +73,47 @@ class osmMap:
 
             # calculate the distance between each two-points
             for u_ref, v_ref in zip(node_list[:-1], node_list[1:]):
-                u_id = u_ref.get("ref")
-                v_id = v_ref.get("ref")
+                u_id = min(u_ref.get("ref"), v_ref.get("ref"))
+                v_id = max(u_ref.get("ref"), v_ref.get("ref"))
 
-                u, u_id = self.nodes[u_id]
-                v, v_id = self.nodes[v_id]
+                try:
+                    index = self.distances[u_id][0].index(v_id)
+                    dist = self.distances[u_id][1][index]
+                except KeyError:  # self.distances[u_id] doesn't exist
+                    u, u_id = self.nodes[u_id]
+                    v, v_id = self.nodes[v_id]
 
-                lat1 = degToRad(float(u.get("lat"))) / 2
-                lat2 = degToRad(float(v.get("lat"))) / 2
-                lon1 = degToRad(float(u.get("lon"))) / 2
-                lon2 = degToRad(float(v.get("lon"))) / 2
+                    dist = self.distance(u, v)
+                    
+                    self.distances[u_id] = ([v_id], [dist])
+                    
+                except ValueError:  # v_id not encountered
+                    u, u_id = self.nodes[u_id]
+                    v, v_id = self.nodes[v_id]
 
-                dlat = lat2-lat1
-                dlon = lon2-lon1
-                a = (pow(sin(dlat)/2, 2)
-                     + cos(lat1)*cos(lat2)*pow(sin(dlon)/2, 2))
-                c = 2*atan2(sqrt(a), sqrt(1-a))
-                dist = R*c
+                    dist = self.distance(u, v)
+                    
+                    self.distances[u_id][0].append(v_id)
+                    self.distances[u_id][1].append(dist)
 
                 # for now, any path is bidirectional
-                edges += "{} {} {}\n".format(u_id, v_id, dist)
-                edges += "{} {} {}\n".format(v_id, u_id, dist)
+                funAdd(u_id, v_id, dist)
+                funAdd(v_id, u_id, dist)
 
-                n_edges += 2
+    def printGraph(self):
+        graph = [""]
 
-        self.graph += "{}\n".format(n_nodes)
-        self.graph += edges
+        def firstLine(s, n_nodes):
+            s[0] += "{}\n".format(n_nodes)
 
-        return self.graph
+        def printEdge(s, u, v, d):
+            s[0] += "{} {} {}\n".format(u, v, d)
 
+        funNNode = lambda n: firstLine(graph, n)
+        funAdd = lambda u, v, d: printEdge(graph, u, v, d)
+
+        self.parse(funNNode, funAdd)
+        return graph[0]
 
 if __name__ == "__main__":
     if not (2 <= len(sys.argv) <= 3):
@@ -100,6 +133,6 @@ if __name__ == "__main__":
         print(graph)
 
     # Calling dijkstra
-    print("Calling dot to process it…")
-    call = Popen(["./dijkstra"], stdin=PIPE, stdout=PIPE)
-    (out, err) = call.communicate(input=bytes(graph, 'UTF-8'))
+    # print("Calling dot to process it…")
+    # call = Popen(["./dijkstra"], stdin=PIPE, stdout=PIPE)
+    # (out, err) = call.communicate(input=bytes(graph, 'UTF-8'))
