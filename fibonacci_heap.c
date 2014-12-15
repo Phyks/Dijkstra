801e6c68ca59185b431de4f3df50e4fc0f41a15f
Phyks <webmaster@phyks.me>
2014-12-15 02:08:08 +0100

    New tests

--- fibonacci_heap.c
+++ fibonacci_heap.c
@@ -230,8 +230,6 @@ fibonacci_heap_element_t *fibonacciHeapExtractMin(fibonacci_heap_t *fh) {
       fibonacciHeapConsolidate(fh);
     }
   }
-  else
-    printf("%d\n", fh->nb_nodes);
 
   return z;
 }
