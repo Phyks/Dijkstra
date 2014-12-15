82bd44ba74831e6c10c601ee7d6b8323b25caac3
Phyks <webmaster@phyks.me>
2014-12-15 03:31:53 +0100

    Random tests and associated data

---
+++ test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import hashlib
+import networkx as nx
+import random
+import subprocess
+import time
+
+#with open("test_out", "a") as fh:
+#    fh.write("|S|\t|A|\tTime")
+
+def h(x):
+    return hashlib.sha224(str(x).encode('utf-8')).hexdigest()
+
+for k in range(100):
+    ident = h(time.time)
+    print("Test "+str(ident))
+    n = random.randint(10, 1000)
+    p = random.uniform(0, 1)
+    G = nx.fast_gnp_random_graph(n, p, directed=True)
+    out = str(n)+" "+str(len(G.edges()))+"\n"
+    print(out.strip())
+    for (i,j) in G.edges():
+        out += str(i)+" "+str(j)+" "+str(random.randint(1, 100))+"\n"
+    with open("tests/random/"+str(ident), "w") as fh:
+        fh.write(out)
+    start = time.time()
+    out = subprocess.check_output(["./dijkstra_opt", "tests/random/"+str(ident)])
+    duration = time.time() - start
+    print(str(duration)+"s")
+    with open("tests_outputs/random/"+str(ident), "wb") as fh:
+        fh.write(out)
+    print()
+
+    with open("test_out", "a") as fh:
+        fh.write("\n"+str(n)+"\t"+str(len(G.edges()))+"\t"+str(duration))
